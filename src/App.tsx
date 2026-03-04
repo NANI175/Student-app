ad0b95509a291ba0cb9107121a6fc42d5c7aa385
NANI175 <sathwiknamani1234@gmail.com>
2026-03-04 18:25:05 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import SplashPage from './pages/SplashPage';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
 import TrackingPage from './pages/TrackingPage';
-
-function App(): JSX.Element {
+function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"         element={<SplashPage />} />
-        <Route path="/login"    element={<LoginPage />} />
-        <Route path="/home"     element={<HomePage />} />
+        <Route path="/" element={<SplashPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/tracking" element={<TrackingPage />} />
-        <Route path="*"         element={<Navigate to="/" replace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
 export default App;
